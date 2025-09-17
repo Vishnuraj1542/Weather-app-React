cff258010c64c9a6305a20d33c010ab86d1305b4
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2025-09-17 17:12:36 +0530

    desgin

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 function App() {
-  const[city,setCity]=useState('Palakkad')
-  // process.env.REACT_APP_API_KEY
-  https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+  const[city,setCity]=useState('')
+
+  const API_KEY = process.env.REACT_APP_API_KEY
+  https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_ KEY}
 
   return (
     <div className="content">
       <h1>Weather</h1>
       <div className="box">
-        <p>Location:</p>
+        <p>Location:{city}</p>
         <p> 18°C </p>
         <p>☀️</p>
+      </div>
+      <div className="search">
+        <input type="text" onChange={(e)=>setCity(e.target.value)}></input>
+        <button onClick={'checkWeather'}>search</button>
       </div>
     </div>
   )
