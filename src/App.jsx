0726b6dc64fe23472bfadcdbb25941f33bd32d5f
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2025-09-20 22:39:14 +0530

    fetch Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import { useState,useEffect } from "react"
 function App() {
   const[city,setCity]=useState('')
+  const[weather,setWeather]=useState(null)
 
-  const API_KEY = process.env.REACT_APP_API_KEY
-  https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_ KEY}
+  const API_KEY = Process.env.REACT_APP_API_KEY
+
+
+ const checkWeather = async()=>{
+  if(!city){
+    return alert('enter a city name')
+  }
+  try{
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${city}&lon={lon}&appid=${API_KEY}`)
+  const data = await response.json()
+
+  if(data.cod === 404){
+    alert('city not found')
+  }
+  setWeather(data)
+  }
+  catch(error){
+    console.error('not found ',error)
+  }
+ }
 
   return (
     <div className="content">
       <h1>Weather</h1>
       <div className="box">
-        <p>Location:{city}</p>
+        <p className="location">Location:{city}</p>
         <p> 18°C </p>
         <p>☀️</p>
       </div>
       <div className="search">
         <input type="text" onChange={(e)=>setCity(e.target.value)}></input>
-        <button onClick={'checkWeather'}>search</button>
+        <button onClick={checkWeather}>search</button>
       </div>
     </div>
   )
